c87b8876ad23672d52530f94b1a919fb65fbbe49
JihedSaad001 <saadjihed@isgs.u-sousse.tn>
2025-02-22 20:43:35 +0100

    rail try 2

--- back/server.js
+++ back/server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("✅ API is running!");
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
