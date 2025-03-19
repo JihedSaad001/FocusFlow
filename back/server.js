e57f813acc7b7f4d8c7322b4e9e5f1526371c560
JihedSaad001 <saadjihed@isgs.u-sousse.tn>
2025-03-19 18:12:35 +0100

    poker real time fix attempt 3

--- back/server.js
+++ back/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Allow frontend origin (adjust for production)
+    origin: "https://focusflow-production.up.railway.app", // Allow frontend origin (adjust for production)
     methods: ["GET", "POST"],
     credentials: true,
   },
