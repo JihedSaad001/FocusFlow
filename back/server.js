54b29871a9b46e3dd83c1e621585ac8898f46ad7
JihedSaad001 <saadjihed@isgs.u-sousse.tn>
2025-03-19 23:20:38 +0100

    poker real time fix attempt 12

--- back/server.js
+++ back/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Allow frontend origin (adjust for production)
+    origin: "https://focus-flow-dusky.vercel.app", // Allow frontend origin (adjust for production)
     methods: ["GET", "POST"],
     credentials: true,
   },
